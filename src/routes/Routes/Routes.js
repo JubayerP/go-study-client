04f15f733b8a1522ab0a9231af2db6f7e9dce9b2
JubayerDev <ajashes858@gmail.com>
2022-12-06 02:46:24 +0600

    404 route

--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -4,6 +4,7 @@ import Blog from "../../pages/Blog/Blog";
 import CheckOut from "../../pages/CheckOut/CheckOut";
 import CourseDetail from "../../pages/CourseDetail/CourseDetail";
 import Courses from "../../pages/Courses/Courses";
+import ErrorPage from "../../pages/ErrorPage/ErrorPage";
 import FAQ from "../../pages/FAQ/FAQ";
 import Home from "../../pages/Home/Home";
 import Login from "../../pages/Login/Login";
@@ -14,6 +15,7 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
